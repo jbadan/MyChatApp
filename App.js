6fe41c35f76686aa792b5741d993e89e5f34a219
jbadan <jenna.badanowski@gmail.com>
2017-11-28 12:55:32 -0800

    reverse scrolling

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, FlatList, TextInput} from 'react-native';
+import { StyleSheet, Text, View, FlatList, TextInput, KeyboardAvoidingView, TouchableOpacity} from 'react-native';
 import {send, subscribe} from 'react-native-training-chat-server';
 
 const NAME = 'Jenna';
@@ -8,6 +8,7 @@ const CHANNEL = 'Reactivate';
 export default class App extends React.Component {
   state = {
     messages: [],
+    typing: "",
   };
 
   componentDidMount() {
@@ -15,6 +16,21 @@ export default class App extends React.Component {
       this.setState({messages});
     });
   }
+  async sendMessage() {
+  // send message to our channel, with sender name.
+  // the `await` keyword means this function execution
+  // waits until the message is sent
+  await send({
+    channel: CHANNEL,
+    sender: NAME,
+    message: this.state.typing
+  });
+
+  // set the component state (clears text input)
+  this.setState({
+    typing: '',
+  });
+}
   renderItem({item}) {
     return (
       <View style={styles.row}>
@@ -25,7 +41,23 @@ export default class App extends React.Component {
   }
   render() {
     return (
-      <FlatList data={this.state.messages} renderItem={this.renderItem} />
+      <View style={styles.container}>
+        <FlatList data={this.state.messages} renderItem={this.renderItem} />
+        <KeyboardAvoidingView behavior="padding">
+          <View style={styles.footer}>
+            <TextInput
+              value={this.state.typing}
+              onChangeText={text => this.setState({typing: text})}
+              style={styles.input}
+              underlineColorAndroid="transparent"
+              placeholder="Type something nice"
+            />
+            <TouchableOpacity onPress={this.sendMessage.bind(this)}>
+              <Text style={styles.send}>Send</Text>
+            </TouchableOpacity>
+          </View>
+        </KeyboardAvoidingView>
+      </View>
     );
   }
 }
@@ -47,5 +79,22 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     paddingRight: 10,
     color: '#456B74',
+  },
+  footer: {
+  flexDirection: 'row',
+  backgroundColor: '#eee',
+  },
+  input: {
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+    fontSize: 20,
+    flex: 1,
+  },
+  send: {
+    alignSelf: 'center',
+    color: 'lightseagreen',
+    fontSize: 16,
+    fontWeight: 'bold',
+    padding: 20,
   },
 });
